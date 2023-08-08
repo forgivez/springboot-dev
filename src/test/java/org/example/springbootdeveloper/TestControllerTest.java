1cd2cf8acc0315669bc29140d8f71a867b2d0836
forgivez <forgivez@naver.com>
2023-08-09 01:56:14 +0900

    three commit

---
+++ src/test/java/org/example/springbootdeveloper/TestControllerTest.java
@@ -0,0 +1,68 @@
+package org.example.springbootdeveloper;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.junit.jupiter.api.Assertions.*;
+/*어노테이션 설명
+* @SpringBootTest : 메인 애플리케이션 클래스에 추가하는 애너테이션인 @SpringBootApplication이 있는 클래스를 찾고 그 클래스에
+* 포함되어있는 빈을 찾은 다음 테스트용 애플리케이션 컨텍스트라는 것을 만드는 용도
+* @AutoConfigureMockMvc : MockMvc를 생성하고 자동으로 구성하는 애너테이션이며 MockMvc는 어플리케이션을 서버에 배포하지 않고도 테스트용
+* MVC 환경을 만들어 요청 및 전송, 응답 기능을 제공하는 유틸리티 클래스. 즉, 컨트롤러를 테스트할때 사용되는 클래스
+* */
+@SpringBootTest
+@AutoConfigureMockMvc
+class TestControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach // 테스트 실행전 실행하는 메서드
+    public void mockMvcSetUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .build();
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        memberRepository.deleteAll();
+    }
+
+    @DisplayName("getAllMembers: 아티클 조회에 성공한다.")
+    @Test
+    public void getAllMembers() throws Exception {
+        //given
+        final String url = "/test";
+        Member savedMember = memberRepository.save(new Member(1L, "홍길동"));
+
+        //when
+        final ResultActions result = mockMvc.perform(get(url)
+                .accept(MediaType.APPLICATION_JSON));
+
+        //then
+        result
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(savedMember.getId()))
+                .andExpect(jsonPath("$[0].name").value(savedMember.getName()));
+    }
+}
