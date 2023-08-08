8fa939a222f04a4a614eae1de12a6df86c34dbec
forgivez <forgivez@naver.com>
2023-08-09 02:28:54 +0900

    test complate + jpa Study

--- src/main/java/org/example/springbootdeveloper/Member.java
+++ src/main/java/org/example/springbootdeveloper/Member.java
@@ -1,21 +1,24 @@
 package org.example.springbootdeveloper;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자
 @AllArgsConstructor
 @Getter
-@Entity
+@Entity // 엔티티로 지정
+/*만약 Entity 애너테이션에 테이블을 지정하고싶다면 하는방법 */
+//@Entity(name = "member_list")
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키 자동으로 1씩 증가
     @Column(name = "id", updatable = false)
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false) // name이라는 not null 컬럼과 매핑
     private String name;
 }
